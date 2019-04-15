8e198c2f46e497611edd44ffe2e144e08442b199
chemaguiniga2 <A01376669@itesm.mx>
2019-04-15 17:53:50 -0500

    Views and server setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const app = express();
+const rootRouter = require('./routes/root');
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded( {extended: false }));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use('/', rootRouter); 
+
+
+/*mongoose.connect(process.env.DB_CON, { useNewUrlParser: true, useCreateIndex: true })
+    .then(() => console.log('Conectado a MongoDB...'))
+    .catch(err => console.log('Error...', err.message));*/
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Escuchando en el puerto ${ port }`));
