001aea580e0f2ddeaf052e66013406fd08d4196d
chemaguiniga2 <A01376669@itesm.mx>
2019-05-05 17:34:12 -0500

    promo page

--- routes/root.js
+++ routes/root.js
@@ -9,6 +9,8 @@ const { Producto, validateProd } = require('../models/producto');
 //const { prodScrap, validateProdScrap } = require('../models/prodScrap');
 const puppeteer = require('puppeteer');
 
+var data = [];
+
 
 
 router.get('/', (req, res) => {
@@ -133,40 +135,6 @@ router.get('/mi_lista', auth, async (req, res) => {
 router.get('/promociones', auth, async (req, res) => {
     let usuario = req.user;
 
-    let data = (async function main() {
-        try {
-            let url = 'https://www.costco.com.mx/Comida-y-Bebida/Cafe-Te-y-Bebidas/Leche/c/cos_6.6.7';
-    
-            let browser = await puppeteer.launch();
-            let page = await browser.newPage();
-    
-            await page.goto(url, {waitUntil: 'networkidle2'});
-            await page.waitForSelector('li.product-item.vline');
-            page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
-           
-            const sec = await page.$$('li.product-item.vline');
-            
-            console.log(sec.length);
-            let prods = [];
-    
-            for (const se of sec) {
-                let dat = {};
-                dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
-                dat.precio = await se.$eval('div.original-price', e => e.innerText);
-                //console.log('name', titulo);
-    
-                prods.push(dat);
-            }
-            
-    
-            await browser.close();
-    
-            return prods;
-        } catch (e) {
-            console.log('Error', e);
-        }
-        
-    })();
 
     res.render('scrapper', {usuario, data});
 
@@ -210,6 +178,42 @@ router.get('/profile', auth, async (req, res) => {
     let usuario = req.user;
     res.render('usuario', { usuario });
 });
+
+(async function getOffers(){
+    try {
+        let url = 'https://www.costco.com.mx/Comida-y-Bebida/Cafe-Te-y-Bebidas/Leche/c/cos_6.6.7';
+
+        let browser = await puppeteer.launch();
+        let page = await browser.newPage();
+
+        await page.goto(url, {waitUntil: 'networkidle2'});
+        await page.waitForSelector('li.product-item.vline');
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
+       
+        const sec = await page.$$('li.product-item.vline');
+        
+        console.log(sec.length);
+        let prods = [];
+
+        for (const se of sec) {
+            let dat = {};
+            dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
+            dat.precio = await se.$eval('div.original-price', e => e.innerText);
+            //console.log('name', titulo);
+
+            prods.push(dat);
+            data.push(dat);
+        }
+        console.log('Entró');
+
+        await browser.close();
+        
+        return prods;
+    } catch (e) {
+        console.log('Error', e);
+    }
+
+})();
 
 
 
