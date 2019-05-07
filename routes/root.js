0394ea7678ad6f2e45b51a9745e72998a1ec05af
chemaguiniga2 <A01376669@itesm.mx>
2019-05-06 19:19:59 -0500

    scrapping part

--- routes/root.js
+++ routes/root.js
@@ -9,7 +9,11 @@ const { Producto, validateProd } = require('../models/producto');
 //const { prodScrap, validateProdScrap } = require('../models/prodScrap');
 const puppeteer = require('puppeteer');
 
-var data = [];
+let prodsRelev = [];
+
+let prodsLacteo = [];
+let prodsDesp = [];
+let prodsKirk = [];
 
 
 
@@ -136,7 +140,7 @@ router.get('/promociones', auth, async (req, res) => {
     let usuario = req.user;
 
 
-    res.render('scrapper', {usuario, data});
+    res.render('scrapper', {usuario, prodsRelev, prodsLacteo, prodsDesp, prodsKirk});
 
 });
 
@@ -179,40 +183,95 @@ router.get('/profile', auth, async (req, res) => {
     res.render('usuario', { usuario });
 });
 
-(async function getOffers(){
+(async function getOffers() {
     try {
-        let url = 'https://www.costco.com.mx/Comida-y-Bebida/Cafe-Te-y-Bebidas/Leche/c/cos_6.6.7';
+        let url = 'https://www.costco.com.mx/';
 
-        let browser = await puppeteer.launch();
+        let browser = await puppeteer.launch({ headless: true });
         let page = await browser.newPage();
 
-        await page.goto(url, {waitUntil: 'networkidle2'});
+        await page.goto(url);
+        await page.waitForSelector('div.carousel-component.costco-carousel-component.clearfix');
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');
+
+        const sec = await page.$$('div.carousel-component.costco-carousel-component.clearfix');
+ 
+        
+        for (const se of sec) {
+            let dat = {};
+            dat.titulo = await se.$eval('div.item-name.ch-name.notranslate', p => p.innerText);
+            dat.precio = await se.$eval('div.original-price', e => e.innerText);
+            prodsRelev.push(dat);
+        }
+        
+        await page.goto('https://www.costco.com.mx/Comida-y-Bebida/Cafe-Te-y-Bebidas/Leche/c/cos_6.6.7');
+
         await page.waitForSelector('li.product-item.vline');
         page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
        
-        const sec = await page.$$('li.product-item.vline');
+        const secLact = await page.$$('li.product-item.vline');
         
-        console.log(sec.length);
-        let prods = [];
 
-        for (const se of sec) {
+
+        for (const se of secLact) {
+            let dat = {};
+            dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
+            dat.precio = await se.$eval('div.original-price', e => e.innerText);
+         
+
+            prodsLacteo.push(dat);
+        }
+ 
+
+        await page.goto('https://www.costco.com.mx/Comida-y-Bebida/Institucional/Despensas/c/cos_6.11.1');
+
+        await page.waitForSelector('li.product-item.vline');
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
+       
+        const secDesp = await page.$$('li.product-item.vline');
+        
+       
+     
+
+        for (const se of secDesp) {
             let dat = {};
             dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
             dat.precio = await se.$eval('div.original-price', e => e.innerText);
             //console.log('name', titulo);
 
-            prods.push(dat);
-            data.push(dat);
+            prodsDesp.push(dat);
         }
-        console.log('Entró');
+ 
+
+
+        await page.goto('https://www.costco.com.mx/Comida-y-Bebida/Kirkland-Signature/c/cos_6.12');
+
+        await page.waitForSelector('li.product-item.vline');
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
+       
+        const secKirk = await page.$$('li.product-item.vline');
+        
+       
+        
+
+        for (const se of secKirk) {
+            let dat = {};
+            dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
+            dat.precio = await se.$eval('div.original-price', e => e.innerText);
+            //console.log('name', titulo);
+
+            prodsKirk.push(dat);
+        }
+
+
+        
 
         await browser.close();
-        
-        return prods;
+
     } catch (e) {
         console.log('Error', e);
     }
-
+    
 })();
 
 
--- scrapper/index.js
+++ scrapper/index.js
@@ -3,49 +3,93 @@ const { prodScrap, validateProdScrap } = require('../models/prodScrap');
 
 (async function main() {
     try {
-        let url = 'https://www.costco.com.mx/Comida-y-Bebida/Cafe-Te-y-Bebidas/Leche/c/cos_6.6.7';
+        let url = 'https://www.costco.com.mx/';
 
-        let browser = await puppeteer.launch();
+        let browser = await puppeteer.launch({ headless: true });
         let page = await browser.newPage();
 
-        await page.goto(url, {waitUntil: 'networkidle2'});
+        await page.goto(url);
+        await page.waitForSelector('div.carousel-component.costco-carousel-component.clearfix');
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');
+
+        const sec = await page.$$('div.carousel-component.costco-carousel-component.clearfix');
+ 
+        let prodsRelev = [];
+        for (const se of sec) {
+            let dat = {};
+            dat.name = await se.$eval('div.item-name.ch-name.notranslate', p => p.innerText);
+            dat.price = await se.$eval('div.original-price', e => e.innerText);
+            prodsRelev.push(dat);
+        }
+        
+        await page.goto('https://www.costco.com.mx/Comida-y-Bebida/Cafe-Te-y-Bebidas/Leche/c/cos_6.6.7');
+
         await page.waitForSelector('li.product-item.vline');
         page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
        
-        const sec = await page.$$('li.product-item.vline');
+        const secLact = await page.$$('li.product-item.vline');
         
-        console.log(sec.length);
-        let prods = [];
 
-        for (const se of sec) {
+        let prodsLacteo = [];
+
+        for (const se of secLact) {
+            let dat = {};
+            dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
+            dat.precio = await se.$eval('div.original-price', e => e.innerText);
+         
+
+            prodsLacteo.push(dat);
+        }
+ 
+
+        await page.goto('https://www.costco.com.mx/Comida-y-Bebida/Institucional/Despensas/c/cos_6.11.1');
+
+        await page.waitForSelector('li.product-item.vline');
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
+       
+        const secDesp = await page.$$('li.product-item.vline');
+        
+       
+        let prodsDesp = [];
+
+        for (const se of secDesp) {
             let dat = {};
             dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
             dat.precio = await se.$eval('div.original-price', e => e.innerText);
             //console.log('name', titulo);
 
-            prods.push(dat);
+            prodsDesp.push(dat);
         }
+ 
+
+
+        await page.goto('https://www.costco.com.mx/Comida-y-Bebida/Kirkland-Signature/c/cos_6.12');
+
+        await page.waitForSelector('li.product-item.vline');
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');;
+       
+        const secKirk = await page.$$('li.product-item.vline');
+        
+       
+        let prodsKirk = [];
+
+        for (const se of secKirk) {
+            let dat = {};
+            dat.titulo = await se.$eval('div.product-name-container', p => p.innerText);
+            dat.precio = await se.$eval('div.original-price', e => e.innerText);
+            //console.log('name', titulo);
+
+            prodsKirk.push(dat);
+        }
+
+
         
 
         await browser.close();
 
-        return prods;
     } catch (e) {
         console.log('Error', e);
     }
     
 })();
 
-    //console.dir(data);
-    /*let producto = new prodScrap({
-        name: data.titulo,
-        categoria: 'Lácteos',
-        precio: data.precio
-
-    });
-
-    await producto.save();
-*/
-    
-
-//})();
